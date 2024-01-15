2204e38e110638cd8637c1c4e27fc06fe66b35ae
EtoOgueji <etoogueji@gmail.com>
2024-01-15 22:46:47 +0100

    doctest example

---
+++ 0x07-test_driven_development/doctest_docstrings.py
@@ -0,0 +1,31 @@
+"""Test can appear in any docstring within the module.
+
+Module-level test cross class and function boundaries
+
+>>> A('a') == B('b')
+False
+"""
+
+class A:
+    """Simple class
+
+    >>> A('instance_name').name
+    'instance_name'
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def method(self):
+        """Returns an unusual value.
+        >>> A('name').method()
+        'eman'
+        """
+        return ''.join(reversed(self.name))
+    
+class B(A):
+    """Another simple class.
+
+    >>> B('different_name').name
+    'different_name'
+    """
